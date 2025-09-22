1577a5769549b26fc5d5c366bf60f402f275bae1
Yumna Ali Muhammad Ali <aliy@informatik.uni-freiburg.de>
2025-09-22 11:18:42 +0200

    helpful scripts for bdd

--- notebooks/copy_vids.py
+++ notebooks/copy_vids.py
@@ -2,10 +2,10 @@ import os
 import shutil
 import pandas as pd
 
-train_csv = "bddx_train_split.csv"
-val_csv = "bddx_val_split.csv"
-train_dir = "/work/dlclarge2/aliy-vjepa/datasets/BDD-X-Dataset/videos/train"
-val_dir = "/work/dlclarge2/aliy-vjepa/datasets/BDD-X-Dataset/videos/val"
+train_csv = "bddx_gps_train_split.csv"
+val_csv = "bddx_gps_val_split.csv"
+train_dir = "/work/dlclarge2/aliy-vjepa/datasets/BDD-X-Dataset_gps/videos/train"
+val_dir = "/work/dlclarge2/aliy-vjepa/datasets/BDD-X-Dataset_gps/videos/val"
 
 os.makedirs(train_dir, exist_ok=True)
 os.makedirs(val_dir, exist_ok=True)
@@ -22,3 +22,17 @@ def copy_files(csv_file, out_dir):
 
 copy_files(train_csv, train_dir)
 copy_files(val_csv, val_dir)
+"""
+def replace_path_column(df, column, new_base_path):
+    df[column] = df[column].apply(lambda x: os.path.join(new_base_path, os.path.basename(x)))
+    return df
+
+
+tr = pd.read_csv("/work/dlclarge2/aliy-vjepa/vjepa2/notebooks/bddx_gps_train_split.csv", header=None, delimiter=" ")
+tr = replace_path_column(tr, 0, "/work/dlclarge2/aliy-vjepa/datasets/BDD-X-Dataset_gps/videos/train")
+tr.to_csv("bddx_gps_train_new_path.csv", index=False, header=False, sep=" ")
+val = pd.read_csv("/work/dlclarge2/aliy-vjepa/vjepa2/notebooks/bddx_gps_val_split.csv", header=None, delimiter=" ")
+val = replace_path_column(val, 0, "/work/dlclarge2/aliy-vjepa/datasets/BDD-X-Datase_gpst/videos/val")
+val.to_csv("bddx_gps_val_new_path.csv", index=False, header=False, sep=" ")
+
+"""
